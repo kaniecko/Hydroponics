6270130be62bd27bb70c7f0e7c5398556e15cb30
kaniecko <anieckok@gmail.com>
2022-12-20 01:35:28 +0000

    19:35 12/19

--- hydroponics/ph_tester.py
+++ hydroponics/ph_tester.py
@@ -2,10 +2,14 @@ import time
 import RPi.GPIO as GPIO
 
 def get_ph():
+    pin_number = 16
     print("getting PH...")
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(4, GPIO.IN)
-    print(GPIO.input(4))
+    print("after setupmode")
+    GPIO.setup(pin_number, GPIO.IN)#, pull_up_down=GPIO.PUD_UP)
+    print("after setup pin IN")
+    print(GPIO.input(pin_number))
     GPIO.cleanup()
+    print("...Done")
 
 get_ph()

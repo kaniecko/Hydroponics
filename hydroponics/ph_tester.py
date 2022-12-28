3fa5ef33cc21ca912091ca6b0d8e3d8c22f1a998
kaniecko <anieckok@gmail.com>
2022-12-28 19:44:12 +0000

    12/28 13:45

--- hydroponics/ph_tester.py
+++ hydroponics/ph_tester.py
@@ -1,15 +1,58 @@
 import time
 import RPi.GPIO as GPIO
+import spidev
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 250000
+calibration = 21.25
+
+def poll_sensor(channel):
+	assert 0 <= channel <= 1, 'ADC channel must be 0 or 1'
+
+	if channel:
+		cbyte = 0b11000000
+	else:
+		cbyte = 0b10000000
+
+	r = spi.xfer2([1,cbyte,0])
+	return ((r[1] & 31) << 6) + (r[2] >> 2)
+
+"""
+try:
+	while True:
+		channel = 0
+		channeldata = poll_sensor(channel)
+
+		voltage = round(((channeldata * 5000) / 1024),0)
+		print('Voltage  (V): {}'.format(voltage))
+		print('Data	    : {}'.format(channeldata))
+
+		ph = -5.70 * (voltage / 1000) + calibration
+		print("ph          :", f"{ph:.2f}")
+
+		print('\n')
+		time.sleep(1)
+finally:
+	spi.close()
+	print('\n All done')"""
+
 
 def get_ph():
-    pin_number = 16
-    print("getting PH...")
-    GPIO.setmode(GPIO.BOARD)
-    print("after setupmode")
-    GPIO.setup(pin_number, GPIO.IN)#, pull_up_down=GPIO.PUD_UP)
-    print("after setup pin IN")
-    print(GPIO.input(pin_number))
-    GPIO.cleanup()
-    print("...Done")
-
-get_ph()
+    	print("getting PH...")
+    	total = 0.0
+    	count = 6.0
+    	try:
+		for x in range(count):
+			channel = 0
+			channeldata = poll_sensor(channel)
+			voltage = round(((channeldata * 5000) / 1024),0)
+			ph = -5.70 * (voltage / 1000) + calibration
+			total = total + ph
+	except:
+		total = -1.0
+		count = 1.0
+		print("An exception has occured")
+    	finally:
+		spi.close()
+    	print("...Done")
+	return total/count

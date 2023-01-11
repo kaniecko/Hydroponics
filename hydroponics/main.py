8b534971b5b5ac78f4d9199f439e695430c2c0b1
LukasVUIC <lvenck2@uic.edu>
2023-01-11 15:37:19 -0600

    1/11/23 15:37

--- hydroponics/main.py
+++ hydroponics/main.py
@@ -11,22 +11,23 @@ def uploadToDataBase(id, phLevel, PPMLevel):
     now = datetime.datetime.now()
     system_instance.importNew(dbConn, id, now, phLevel, PPMLevel)
 
-try:
-    hydroponicssheet = sheet_editor.give_sheet_connection()
-    while True:
-        print("Starting Wait...")
-        time.sleep(300)
-        print("Wait is done...")
-        ph = ph_tester.get_ph()
-        print("the ph retrieved is:",f"{ph:.2f}")
-        # id that was on the google sheet
-        id = sheet_editor.push_data(ph, hydroponicssheet)
-        print("sheet updated with id#:",id)
-        uploadToDataBase(id, ph, 0)
-        print("database updated with id#:",id)
-except:
-    print("There was an error.")
-    time.sleep(600)
-finally:
-    print("Done!")
+while True:
+    try:
+        hydroponicssheet = sheet_editor.give_sheet_connection()
+        while True:
+            print("Starting Wait...")
+            time.sleep(300)
+            print("Wait is done...")
+            ph = ph_tester.get_ph()
+            print("the ph retrieved is:",f"{ph:.2f}")
+            # id that was on the google sheet
+            id = sheet_editor.push_data(ph, hydroponicssheet)
+            print("sheet updated with id#:",id)
+            uploadToDataBase(id, ph, 0)
+            print("database updated with id#:",id)
+    except:
+        print("There was an error.")
+        time.sleep(600)
+    finally:
+        print("Done!")
 
